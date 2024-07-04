0c614a4826e0c7c6b2eeb90a7550334e5da6526a
Olazykute <vianney.saadetian@estaca.eu>
2024-07-04 23:52:04 +0200

    Added GPS class, need airborne setup

--- main.py
+++ main.py
@@ -42,13 +42,77 @@ class Lora:
         self.lora.reset()
         print("LoRa Reset")
         
+class GPS:
+    def __init__(self):
+        self.uart = machine.UART(1, baudrate=9600, tx=8, rx=9) #UART 2 tx=8, rx=9
+        self.TIMEOUT = False
+        self.FIX_STATUS = False
+        self.latitude = ""
+        self.longitude = ""
+        self.satellites = ""
+        self.GPStime = ""
+        self.buffer = bytearray(255)
+        
     
+    def convertToDegree(self, RawDegrees):
+        
+        RawAsFloat = float(RawDegrees)
+        firstdigits = int(RawAsFloat/100) 
+        nexttwodigits = RawAsFloat - float(firstdigits*100) 
+        
+        Converted = float(firstdigits + nexttwodigits/60.0)
+        Converted = '{0:.6f}'.format(Converted) 
+        return str(Converted)    
+    
+    def getGPS(self):
+        timeout = utime.time() + 8 
+        while True:
+            self.uart.readline()
+            self.buffer = str(self.uart.readline())
+            parts = self.buffer.split(',')
+        
+            if (parts[0] == "b'$GPGGA" and len(parts) == 15):
+                if(parts[1] and parts[2] and parts[3] and parts[4] and parts[5] and parts[6] and parts[7]):
+                    print(self.buffer)
+                    
+                    self.latitude = self.convertToDegree(parts[2])
+                    if (parts[3] == 'S'):
+                        self.latitude = "-" + self.latitude
+                    self.longitude = self.convertToDegree(parts[4])
+                    if (parts[5] == 'W'):
+                        self.longitude = "-" + self.longitude
+                    self.satellites = parts[7]
+                    self.GPStime = parts[1][0:2] + ":" + parts[1][2:4] + ":" + parts[1][4:6]
+                    self.FIX_STATUS = True
+                    break
+                    
+            if (utime.time() > timeout):
+                self.TIMEOUT = True
+                break
+            utime.sleep_ms(500)
+    
+    def printGPS(self):
+        if(self.FIX_STATUS == True):
+            print("Printing GPS data...")
+            print(" ")
+            print(f"Latitude: {self.latitude}")
+            print(f"Longitude: {self.longitude}")
+            print(f"Satellites: {self.satellites}")
+            print(f"Time: {self.GPStime}")
+            print("----------------------")
+            
+            #FIX_STATUS = False
+            
+        if(self.TIMEOUT == True):
+            print("No GPS data is found.")
+            self.TIMEOUT = False
+            
 
-#main function
+
 def main():
     print("Hello World!")
-    #code here
+    
 
-#run main function
+
 if __name__ == "__main__":
     main()  
